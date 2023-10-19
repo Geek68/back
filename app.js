4ba66452b40633d9b2969cd4c813f7a5ab148935
RajoRabeza <rajorabeza@gmail.com>
2023-10-19 22:37:01 +0300

    create Levels if not exist

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 const corOptions = {
-  origin:`http://localhost:${port}`
-}
+  origin: `http://localhost:${port}`,
+};
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false } ))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
   );
   next();
 });
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-const db = require('./models')
-const { testParcours } = require('./middlewares/testData')
-db.sequelize.sync({force: false}).then(()=>{
-    console.log("DB sync");
+const db = require("./models");
+const { handleStart } = require("./middlewares/testData");
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("DB sync");
 });
 
-testParcours()
-
-
-
+handleStart();
 
 //routes
-app.use('/api/login', require('./routes/auth.routes'))
-app.use('/api/user', require('./routes/user.routes'))
-app.use('/api/student', require('./routes/student.routes'))
-app.use('/api/prof', require('./routes/prof.routes'))
-app.use('/api/parcours', require('./routes/parcours.routes'))
-app.use('/api/salle', require('./routes/salle.routes'))
-app.use('/api/seance', require('./routes/seance.routes'))
-app.use('/api/niveau', require('./routes/level.routes'))
-app.use('/api/matiere', require('./routes/matiere.routes'))
+app.use("/api/login", require("./routes/auth.routes"));
+app.use("/api/user", require("./routes/user.routes"));
+app.use("/api/student", require("./routes/student.routes"));
+app.use("/api/prof", require("./routes/prof.routes"));
+app.use("/api/parcours", require("./routes/parcours.routes"));
+app.use("/api/salle", require("./routes/salle.routes"));
+app.use("/api/seance", require("./routes/seance.routes"));
+app.use("/api/niveau", require("./routes/level.routes"));
+app.use("/api/matiere", require("./routes/matiere.routes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- middlewares/testData.js
+++ middlewares/testData.js
@@ -1,28 +1,48 @@
-const db = require('../models')
-const {Parcours, Niveau} = db
-const asyncHandler = require('express-async-handler')
+const db = require("../models");
+const { Parcours, Niveau } = db;
+const asyncHandler = require("express-async-handler");
 
-const testParcours = asyncHandler(async () => {
+const handleStart = asyncHandler(async () => {
+  const all_parcours = await Parcours.findAll();
+  if (!all_parcours.length) {
+    Parcours.bulkCreate([
+      { designation: "PRO" },
+      { designation: "GB" },
+      { designation: "SR" },
+      { designation: "IG" },
+      { designation: "L1Pro" },
+    ])
+      .then(() => {
+        console.log("parcours ajouté avec succès");
+      })
+      .catch((err) => {
+        console.log(err.parent.detail);
+      });
+  } else {
+    console.log("efa misy parcours ao");
+  }
 
-    const all_parcours = await Parcours.findAll()
-    if(all_parcours.length === 0 ){
-        const parcours = await Parcours.bulkCreate([
-            { designation: 'PRO' },
-            { designation: 'GB' },
-            { designation: 'SR' },
-            { designation: 'IG' },
-          ]).then(()=>{
-            console.log('parcours ajouté avec succès')
-         }).catch(err => {
-            console.log(err.parent.detail)
-         })
+  Niveau.findAll().then((levels) => {
+    if (!levels.length) {
+      Niveau.bulkCreate([
+        { designation: "L1" },
+        { designation: "L2" },
+        { designation: "L3" },
+        { designation: "M1" },
+        { designation: "M2" },
+      ])
+        .then(() => {
+          console.log("niveau ajouté avec succès");
+        })
+        .catch((err) => {
+          console.log(err.parent.detail);
+        });
     } else {
-        console.log('efa misy parcours ao')
+      console.log("efa misy niveau ao");
     }
-}
-)
-
+  });
+});
 
 module.exports = {
-    testParcours
-}
+  handleStart,
+};
