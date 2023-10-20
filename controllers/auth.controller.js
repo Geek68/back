638d53f838f0ce05941f29ae7aedb6ff0dbb53eb
RajoRabeza <rajorabeza@gmail.com>
2023-10-20 12:12:04 +0300

    login changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,26 +1,28 @@
-const bcrypt = require('bcryptjs')
-const db = require('../models')
+const bcrypt = require("bcryptjs");
+const db = require("../models");
 
-const {UserAccount} = db
+const { UserAccount } = db;
 
-exports.signin = async (req,res)=>{
-    let {_login, _password} = req.body
+exports.signin = async (req, res) => {
+  let { login, password } = req.body;
 
-    await UserAccount.findOne({
-        where: {
-            login: _login
+  await UserAccount.findOne({
+    where: {
+      login,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not Found." });
+      } else {
+        if (!bcrypt.compareSync(password, user.password)) {
+          res.status(401).json({ message: "Invalid Password" });
+        } else {
+          res.status(200).json({ message: "OK", data: user });
         }
-    }).then(user => {
-        if(!user) {
-            return res.status(404).send({message: "User Not Found."});
-        }else{
-            if(!(bcrypt.compareSync(_password,user.password))){
-                res.status(401).json({message: 'Invalid Password'})
-            }else{
-                res.status(200).json({message: 'OK', data: user})
-            }
-        }
-    }).catch(err =>{
-        res.status(500).send({message: err});
+      }
     })
-}
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
+};
