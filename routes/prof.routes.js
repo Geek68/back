52d4f939e3b12e0734f70a376eacb9386727b4f3
Imarioa <elieimario1@gmail.com>
2023-10-23 10:56:10 +0300

    separate upload photo

--- routes/prof.routes.js
+++ routes/prof.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { InitCreateProf, FindProfById, FindProf, UpdateProf, DeleteProf } = require('../controllers/prof.controller');
+const { InitCreateProf, FindProfById, FindProf, UpdateProf, DeleteProf, UpadteProfProfPic } = require('../controllers/prof.controller');
 const { uploadXlsxFile } = require('../middlewares/uploadExcelFile')
 const { upload } = require('../middlewares/uploadImgMiddleware');
 
 router.route('/').get(FindProf)
 router.route('/uploadProf').post(uploadXlsxFile, InitCreateProf)
-router.route('/:id').get(FindProfById).put(upload,UpdateProf).delete(DeleteProf)
+router.route('/updateProfilPic/:id').put(upload, UpadteProfProfPic)
+router.route('/:id').get(FindProfById).put(UpdateProf).delete(DeleteProf)
 
 
 module.exports = router;
