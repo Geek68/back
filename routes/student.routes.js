708ea7f527aeeab568870b3c4c5112815e63abc3
Threesten <ramiandrisoath@gmail.com>
2023-10-23 11:31:21 +0300

    student update profile pic misaraka

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db.config');
-const { getAllStudents, getOneStudent, postStudent, updateStudent, deleteStudent, getPromotion } = require('../controllers/student.controller');
+const { getAllStudents, getOneStudent, postStudent, updateStudent, deleteStudent, getPromotion, updateStudentPic } = require('../controllers/student.controller');
 const { upload } = require('../middlewares/uploadImgMiddleware');
 
 
 router.route('/').get(getAllStudents).post(upload, postStudent)
 router.route('/:id').get(getOneStudent).put(upload, updateStudent).delete(deleteStudent)
 router.route('/promotion').get(getPromotion)
+router.route('/updateStudentPic/:id').put(upload, updateStudentPic)
+
 module.exports = router;
