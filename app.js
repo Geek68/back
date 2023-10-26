4af84c258057ce284c2ee92f5be6aca429d87562
Threesten <ramiandrisoath@gmail.com>
2023-10-26 22:58:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(express.static("./public"));
 const db = require('./models')
 
-db.sequelize.sync({force: true, alter:true}).then(()=>{
+db.sequelize.sync({force: false, alter:true}).then(()=>{
   console.log('Db synced')
   // handleStart();
 });
@@ -31,12 +31,12 @@ db.sequelize.sync({force: true, alter:true}).then(()=>{
 //routes
 app.use("/api/login", require("./routes/auth.routes"));
 app.use("/api/user", require("./routes/user.routes"));
-app.use("/api/student", require("./routes/etudiant.routes"));
+app.use("/api/etudiant", require("./routes/etudiant.routes"));
 app.use("/api/prof", require("./routes/prof.routes"));
 app.use("/api/salle", require("./routes/salle.routes"));
-app.use("/api/seance", require("./routes/tranchehoraire.routes"));
+app.use("/api/tranchehoraire", require("./routes/tranchehoraire.routes"));
 app.use("/api/niveau", require("./routes/niveau.routes"));
-app.use("/api/matiere", require("./routes/ec.routes"));
+app.use("/api/ec", require("./routes/ec.routes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
