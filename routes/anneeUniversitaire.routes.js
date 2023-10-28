b16f52cd19c5f087b8685ca8cedf68f6bcdef1e8
Threesten <ramiandrisoath@gmail.com>
2023-10-28 23:42:59 +0300

    semestre_anneeUniv Ok

---
+++ routes/anneeUniversitaire.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db.config');
+const { getAllAnneeUniversitaire, getOneAnneeUniversitaire, postAnneeUniversitaire, updateAnneeUniversitaire, deleteAnneeUniversitaire } = require('../controllers/AnneeUniversitaire.controller');
+
+
+router.route('/').get(getAllAnneeUniversitaire).post(postAnneeUniversitaire)
+router.route('/:id').get(getOneAnneeUniversitaire).put(updateAnneeUniversitaire).delete(deleteAnneeUniversitaire)
+module.exports = router;
