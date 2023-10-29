c9d4807468379cb254afec661a38c3981265f007
Threesten <ramiandrisoath@gmail.com>
2023-10-29 17:55:54 +0300

    route

--- routes/tranchehoraire.routes.js
+++ routes/tranchehoraire.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db.config');
-const { getAllSeance, getOneSeance, postSeance, updateSeance, deleteSeance } = require('../controllers/tranchehoraire.controller');
+const { getAllTrancheHoraire, getOneTrancheHoraire, postTrancheHoraire, updateTrancheHoraire, deleteTrancheHoraire } = require('../controllers/tranchehoraire.controller');
 
 
-router.route('/').get(getAllSeance).post( postSeance)
-router.route('/:id').get(getOneSeance).put( updateSeance).delete(deleteSeance)
+router.route('/').get(getAllTrancheHoraire).post( postTrancheHoraire)
+router.route('/:id').get(getOneTrancheHoraire).put( updateTrancheHoraire).delete(deleteTrancheHoraire)
 module.exports = router;
